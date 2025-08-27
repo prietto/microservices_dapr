6da147f2603f8cc1304a7b396fd733490a7404b2
Luis Prieto <luis.prieto@siesa.com>
2025-08-27 14:44:08 -0500

    changes

--- inventory_service/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import Base
-from .inventory_item import InventoryItem
-from .category import Category
-
-# Exportar todos los modelos
-__all__ = ["Base", "InventoryItem", "Category"]
--- inventory_service/run-inventory-with-dapr.ps1
+++
@@ -1,7 +0,0 @@
-dapr run `
-  --app-id inventory-service `
-  --app-port 8000 `
-  --dapr-http-port 3500 `
-  --dapr-grpc-port 50001 `
-  --components-path ./components `
-  -- python -m app.main
